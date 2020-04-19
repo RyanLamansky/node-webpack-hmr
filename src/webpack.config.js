71ec42d4193ff80b9a97ff5f5523a9c694313038
Ryan Lamansky <kardax@kardax.com>
2020-04-18 22:39:19 -0500

    Server bundle now uses ES2019 syntax, fixed production launch. use stricter TypeScript settings.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,9 +3,27 @@
 const { resolve } = require('path');
 const { HotModuleReplacementPlugin, } = require('webpack');
 
- const isDevMode = process.env.NODE_ENV !== 'production';
+const isDevMode = process.env.NODE_ENV !== 'production';
 
 console.log(`Webpack running in ${isDevMode ? 'development' : 'production'} mode.`)
+
+const createModule = (target = "ES5") => ({
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              target,
+            }
+          }
+        }
+      ],
+    }
+  ]
+});
 
 /** @type {import("webpack").Configuration} */
 const commonConfig = {
@@ -14,16 +32,12 @@ const commonConfig = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
-  },
 };
 
 /** @type {import("webpack").Configuration} */
 const clientConfig = {
   ...commonConfig,
+  module: createModule(),
   entry: isDevMode ? ['webpack-hot-middleware/client', './client.ts'] : ['./client.ts'],
   output: {
     path: resolve(__dirname, isDevMode ? 'client' : 'build/client'),
@@ -35,6 +49,7 @@ const clientConfig = {
 /** @type {import("webpack").Configuration} */
 const serverConfig = {
   ...commonConfig,
+  module: createModule("ES2019"),
   entry: isDevMode ? './server.ts' : './productionStart.ts',
   output: {
     libraryTarget: 'commonjs',
