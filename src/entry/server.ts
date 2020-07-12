50883769e3e5f1bbef973f75b77e95051dd904f3
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2020-07-12 18:18:31 -0500

    Fixed stray closing script tag, fully pre-generate home page.

--- src/entry/server.ts
+++ src/entry/server.ts
@@ -6,13 +6,16 @@ import { createHash } from "crypto";
 const checkCompatibilityCode = "(async()=>{})();window['.Modern']=1;";
 const checkCompatibilityName = `/checkCompatibility-${createHash("md5").update(checkCompatibilityCode).digest("hex")}.js`;
 
-const bundleLoader = `<script defer onload="var s=document.createElement('script');s.src=(window['.Modern']?'/modern':'/legacy')+'.js?${BUILD_DATE}';s.defer=true;document.body.appendChild(s);" src="${checkCompatibilityName}"></script>`;
+const homePage = (() => {
+  const bundleLoader = `<script defer onload="var s=document.createElement('script');s.src=(window['.Modern']?'/modern':'/legacy')+'.js?${BUILD_DATE}';s.defer=true;document.body.appendChild(s);" src="${checkCompatibilityName}"></script>`;
+  return `<!DOCTYPE html><html><head><meta charset="utf-8" /><title>Experimental</title></head><body><div id="app-root">Experimental</div>${bundleLoader}</body></html>`;
+})();
 
 export default function server(request: Request, response: Response): void | Promise<void> {
   try {
     switch (request.path) {
     case "/":
-      response.send(`<!DOCTYPE html><html><head><meta charset="utf-8" /><title>Experimental</title></head><body><div id="app-root">Experimental</div>${bundleLoader}</script></body></html>`);
+      response.send(homePage);
       return;
     case checkCompatibilityName:
       response.setHeader("Cache-Control", "public, max-age=604800, no-transform, immutable");
